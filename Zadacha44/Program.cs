ec3c46cfd0c87c597ffd1d5c41252cdc473a2760
Kirill Karavaev <coffee.2016@yandex.ru>
2023-01-21 17:24:15 +0300

    Работа в классе

---
+++ Zadacha44/Program.cs
@@ -0,0 +1,27 @@
+﻿//Вывод первых чисел Фибоначчи без рекурсии
+int[] Task44()
+{
+    Console.WriteLine("Введите число N");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {   
+        sum = sum +i;
+        array[i] = sum;
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("");
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+int[] Array = Task44();
+int N = Convert.ToInt32(Console.ReadLine());
+ShowArray(Array);
