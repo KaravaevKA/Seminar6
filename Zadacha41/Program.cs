793cb5e7e4a779ce85e76b2630d3255d08988325
Kirill Karavaev <coffee.2016@yandex.ru>
2023-01-21 18:21:09 +0300

    Добавлено решение задачи 41  из дз

---
+++ Zadacha41/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] CreateArray()
+{
+    Console.WriteLine("Введите количество элементов массива");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {   
+        Console.WriteLine($"Введите {i+1} элемент массива");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void CountPositive(int[] array)
+{
+    int count = 0;
+    foreach (var i in array)
+    {
+        if (i > 0) count ++;
+    
+    }
+    Console.WriteLine($"Пользователь ввел {count} положительных элементов");
+}
+
+int[] Array = CreateArray();
+ShowArray(Array);
+CountPositive(Array);
