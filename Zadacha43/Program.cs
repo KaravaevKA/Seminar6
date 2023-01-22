fcbe4ab8a897cd88d724552bd7692eee3109e9b0
Kirill Karavaev <coffee.2016@yandex.ru>
2023-01-22 15:01:14 +0300

    Добавлено решение задачи 43 из дз

---
+++ Zadacha43/Program.cs
@@ -0,0 +1,18 @@
+﻿//Вывод точки пересечения по коэффициентам
+Console.WriteLine("Заданы две прямые вида y=kx+b. Необходимо определить координаты точки пересечения");
+Console.WriteLine("Введите b1");
+double BFirst = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k1");
+double KFirst = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2");
+double BSecond = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2");
+double KSecond = Convert.ToDouble(Console.ReadLine());
+
+double X = (BSecond-BFirst)/(KFirst-KSecond);
+double Y = (KFirst*(BSecond-BFirst)/(KFirst-KSecond)) + BFirst;
+Console.Write("(");
+Console.Write(X);
+Console.Write(";");
+Console.Write(Y);
+Console.Write(")");
